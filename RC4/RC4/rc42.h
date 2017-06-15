e89c3ba383cb17278817acdd09956e0808258ce8
aleksis059 <aleksis059@mail.ru>
2017-06-15 15:20:12 +0500

    Create rc42.h

--- RC4/RC4/rc42.h
+++ RC4/RC4/rc42.h
@@ -4,23 +4,27 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <math.h>
+#include "../../files/MyLib.h"
 class RC4
 {
-private:
+public:
+    RC4(){}
     unsigned char mass[256];
     unsigned int size, i, j;
     unsigned char tmp;
     int Ki;
     std::vector<unsigned char>EnData;
     std::vector<unsigned char>DeData;
-    unsigned char get();
-    void Meta(unsigned char *key);
-public:
-    RC4(){}
     void Dec(unsigned char *key, std::vector<unsigned char> DataVector);
     void Enc(unsigned char *key, std::vector<unsigned char> DataVector);
+    unsigned char get();
+    void Meta(unsigned char *key);
     std::vector<unsigned char>&EncData();
     std::vector<unsigned char>&DecData();
+   
+    
+    
     
 
 };
